2a03a330d20bbcac496d2bc41385ab21cde76a76
zMarcio <marciopaulolima21@edu.unifor.br>
2023-03-21 14:05:34 -0300

    faq page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Home from './Home';
+import Faq from './Faq';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Faq/>
+    </div>
+  );
+}
+
+export default App;
