e29d7361038114f121acc033fcf92f0f6222d95a
phln1108 <109960325+phln1108@users.noreply.github.com>
2023-04-02 21:54:01 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,43 +3,17 @@ import {Routes, Route, Link } from 'react-router-dom';
 
 import Home from './Home';
 import Faq from './Faq';
+import NavBar from './NavBar';
+import Playlist from './Playlist';
 
 function App() {
   return (
     <div className="App">
-
-
-<nav class="navbar navbar-inverse ">
-      <div class="container-fluid">
-          <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="home.html">
-                  <img src="imgs/logo_colorido_semFundo.png" width="40" style={{marginTop: -10}} alt=""/>
-              </a>
-              <a class="navbar-brand" href="home.html">Soptfy</a>
-          </div>
-          <div class="collapse navbar-collapse" id="myNavbar">
-              <ul class="nav navbar-nav">
-                  <li><Link to="/Home">Home</Link></li>
-                  <li class="active"><Link to="/Faq">Suporte</Link></li>
-              </ul>
-              <ul class="nav navbar-nav navbar-right">
-                  <li><Link to="forms.html"><span class="glyphicon glyphicon-user"></span> Sign Up</Link></li>
-                  <li><Link to="login.html"><span class="glyphicon glyphicon-log-in"></span> Login</Link></li>
-                  
-              </ul>
-          </div>
-      </div>
-  </nav>
-
-
+    <NavBar></NavBar>
       <Routes>
         <Route path='/Home' element={<Home/>}></Route>
         <Route path='/Faq' element={<Faq/>}></Route>
+        <Route path='/Playlist' element={<Playlist/>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Playlist.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Playlist.css"
+import { Link } from "react-router-dom";
+
+import Angra from "./assets/images/angra.png";
+import A7x from "./assets/images/a7x.png";
+import Racionais from "./assets/images/racionais.jpg";
+// import playlists from "./Playlists.json"
+
+const Playlist = props => {
+
+    const playlists = [{
+        nome: "Racionais",
+        capa: Racionais,
+        musicas: ["nome"]
+    },
+    {
+        nome: "a7x",
+        capa: A7x,
+        musicas: ["nome"]
+    },
+    {
+        nome: "angra",
+        capa: Angra,
+        musicas: ["nome"]
+    }]
+
+    const list_playlists = playlists.map(
+        (p) =>
+            <li className="play">
+                <Link to={p.nome}>
+                <img className="playlists" src={p.capa} alt={p.nome} />
+                <h4>{p.nome}</h4>
+                </Link>
+            </li>
+    )
+
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-2"></div>
+                <div className="interface col-sm-8 well well-md">
+                    <div className="row">
+                        <h1>Playlist</h1>
+                    </div>
+                    <div className="row" id="rowcenter">
+                        <div className="col-sm-4"></div>
+                        <div className="col-sm-7">
+                            <ul>
+                                {list_playlists}
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div className="col-sm-2"></div>
+                </div>
+
+            </div>
+        </div >
+    )
+}
+
+export default Playlist
